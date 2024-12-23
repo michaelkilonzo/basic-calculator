c800e02c3f83f0869605b9fefa69380b90b17df4
Michael Kilonzo <68613943+michaelkilonzo@users.noreply.github.com>
2024-12-23 11:54:28 -0500

    proper styling

--- scripts.js
+++ scripts.js
@@ -3,12 +3,18 @@
 const buttons = document.querySelectorAll('button'); // Select all button elements
 // Flags
 // Functions 
+function calculate(input) {
+    input = parseInt(input) || input;
+    console.log(input)
+}
 // Event Listeners 
 // Add event listeners to each button
 buttons.forEach(button => {
     button.addEventListener('click', (event) => {
-        console.log(`${event.target.textContent}`);
+        let input = event.target.textContent
+        calculate(input);
     });
 });
 
 // Driver 
+let stack = [];
